54fc0cae716c67ea4a8b805fce775b91e23666c7
Yuriel849 <Yuriel849SP@gmail.com>
2023-12-08 10:44:40 +0900

    Fixed errors in test file

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	printf("This is a test file created to be the first code committed in this repository and has no other purpose"\n);
+	printf("This is a test file created to be the first code committed in this repository and has no other purpose\n");
 
 	return 0;
 }
